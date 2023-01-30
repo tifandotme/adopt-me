16b2336e4e11402b69d42a3713116f4c3b878c93
Tifan <tifan@proton.me>
2023-01-30 09:17:15 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
+
+export default App;
