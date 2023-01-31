7b4950189db4217ef1e33e9489313d66b33953f8
Tifan <tifan@proton.me>
2023-01-31 17:27:51 +0700

    another one

---
+++ src/fetchPet.js
@@ -0,0 +1,13 @@
+async function fetchPet({ queryKey }) {
+  const id = queryKey[1];
+
+  const apiRes = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
+
+  if (!apiRes.ok) {
+    throw new Error("Failed to fetch pet");
+  }
+  
+  return apiRes.json();
+}
+
+export default fetchPet;
